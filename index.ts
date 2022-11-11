654516ee21d0605424245cf07e0000e2b0ad1c98
Eleri <n.erik.jak@gmail.com>
2022-11-11 00:17:04 -0300

    commit random

--- index.ts
+++ index.ts
@@ -6,3 +6,19 @@ const app = express()
 app.listen(port, ()=>{
     console.log('listening on port: ' + port);
 })
+app.get("/users", (req, res)=>{
+    res.json({
+        users:[]
+    })
+})
+app.post("/users", (req, res)=>{
+    res.status(201).json({
+        "message": "ok",
+        "id":1234
+    })
+})
+app.get("/users/:id/product", (req, res)=>{
+    res.json({
+        products:[]
+    })
+})
